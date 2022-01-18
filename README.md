f9977b9f5ec10e530894ae680288152b6f6902df
Александр <alextrexen@gmail.com>
2022-01-18 16:48:17 +0300

    Планшетная версия главной страницы

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Flat12
+
+This is test task for FLat12.
+
+Breakpoints:
+____________
+320px, 768px, 1024px, 1280px, 1440px, 1920px
--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,6 +1,7 @@
 const menuButton = document.querySelector('.main-nav__menu-button');
 const menu = document.querySelector('.main-nav__nav-list');
 
+// Мобильное меню
 menuButton.onclick = () => {
   menu.classList.toggle('main-nav__nav-list--open');
 }
