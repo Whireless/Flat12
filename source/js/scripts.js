0a43e703f1895a8025b76d646a41501cc338caec
Александр <alextrexen@gmail.com>
2022-01-18 18:38:19 +0300

    Планшетная версия для страницы редактирования

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,7 +1,14 @@
+const page = document.querySelector('body');
 const menuButton = document.querySelector('.main-nav__menu-button');
 const menu = document.querySelector('.main-nav__nav-list');
 
 // Мобильное меню
 menuButton.onclick = () => {
   menu.classList.toggle('main-nav__nav-list--open');
+
+  if (menu.classList.contains('main-nav__nav-list--open')) {
+    page.style.overflow = 'hidden';
+  } else {
+    page.removeAttribute('style');
+  }
 }
